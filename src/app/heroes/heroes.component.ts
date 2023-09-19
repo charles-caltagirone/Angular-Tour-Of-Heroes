b4472a1d55d6cce9d84beaab325235407990bb77
CharlesCaltagirone <charles.dewidehem-caltagirone@laplateforme.io>
2023-09-19 16:40:55 +0200

    création du service Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
 
-  heroes = HEROES;
+export class HeroesComponent {
   
   selectedHero?: Hero;
+  
+  heroes: Hero[] = [];
 
+  constructor(private heroService: HeroService) {
+  }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }   
